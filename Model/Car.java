55a805ea18e228ad8e339db65e9f4f36352bc9de
hanselmito <antonio@gmail.com>
2023-11-24 10:11:06 +0100

    primer commit

---
+++ Model/Car.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Car {
+    private String licence;
+    private String model;
+    private int age;
+
+    public Car(String licence, String model, int age) {
+        this.licence = licence;
+        this.model = model;
+        this.age = age;
+    }
+    
+}
